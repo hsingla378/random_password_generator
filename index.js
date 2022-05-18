6aba05179a1be1398549923e1e5d0be2fd992edf
hsingla378 <hsingla378@gmail.com>
2022-05-18 22:19:05 +0530

    used HTML, CSS and JavaScript

---
+++ index.js
@@ -0,0 +1,18 @@
+function generatePassword(){
+    let length = 15
+    let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+    let genPass = ""
+    for(let i = 0; i < length; ++i){
+        genPass += charset.charAt(Math.floor(Math.random() * charset.length))
+        }
+
+    return genPass
+}
+
+function generatePasswords(){
+    document.getElementById("generatedPassword1").textContent = generatePassword()
+    document.getElementById("generatedPassword2").textContent = generatePassword()
+    document.getElementById("generatedPassword3").textContent = generatePassword()
+    document.getElementById("generatedPassword4").textContent = generatePassword()
+
+}
