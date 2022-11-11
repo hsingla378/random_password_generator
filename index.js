61ce44e147a468c0afb2aadd290d4873d548a14a
hsingla378 <hsingla378@gmail.com>
2022-11-11 12:41:59 +0530

    position at center

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-function generatePassword(){
-    let length = 15
-    let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
-    let genPass = ""
-    for(let i = 0; i < length; ++i){
-        genPass += charset.charAt(Math.floor(Math.random() * charset.length))
-        }
+function generatePassword() {
+  let length = 15;
+  let charset =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+  let genPass = "";
+  for (let i = 0; i < length; ++i) {
+    genPass += charset.charAt(Math.floor(Math.random() * charset.length));
+  }
 
-    return genPass
+  return genPass;
 }
 
-function generatePasswords(){
-    document.getElementById("generatedPassword1").textContent = generatePassword()
-    document.getElementById("generatedPassword2").textContent = generatePassword()
-    document.getElementById("generatedPassword3").textContent = generatePassword()
-    document.getElementById("generatedPassword4").textContent = generatePassword()
-
+function generatePasswords() {
+  document.getElementById("generatedPassword1").textContent =
+    generatePassword();
+  document.getElementById("generatedPassword2").textContent =
+    generatePassword();
+  document.getElementById("generatedPassword3").textContent =
+    generatePassword();
+  document.getElementById("generatedPassword4").textContent =
+    generatePassword();
 }
